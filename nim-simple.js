f733b9e9bb1a766bf02fc74a6a6a035234b8aae9
adutilbtc <42972863+adutilbtc@users.noreply.github.com>
2019-11-20 11:38:11 -0500

    Create nim-simple.js

---
+++ nim-simple.js
@@ -0,0 +1,82 @@
+/*nim simple by Andrew*/
+var games=0;
+var again=true;
+while(again==true){
+	alert("welcome to nim");
+	var count=0;
+	var turns=0;
+	var next=games%2;
+	while(count<21){
+		
+		if(next==0){
+			count=cpuTurn(count);
+			
+		}
+		else if (next==1){ 
+			count=userTurn(count);
+		}
+		next=nextSwitch(next);
+	}
+	declareWinner(next);
+	playAgain(again);
+}	
+function cpuTurn(count){
+	var cpuInput=Math.floor(Math.random()*3)+1;
+	if(count+cpuInput>22){
+	 cpuInput=1;
+	}
+	alert("cpu counts "+cpuInput);
+	count+=cpuInput;
+	alert("current count is "+count);
+	return count;
+	
+}
+function userTurn(count){
+	var userInput=prompt("enter number 1-3");
+	if(userInput!="1"&&userInput!="2"&&userInput!="3"){
+		alert("invalid input, cheater");
+	}
+	else{
+		count+=userInput;
+		alert("current count is "+count);
+		return count;
+	}
+}
+
+
+
+function nextSwitch(next){
+	alert("working");
+	if(next==0){
+		return 1;
+	}
+	else if (next==1){
+		return 0;
+	}
+	
+}
+
+
+function declareWinner(next){
+	if(next==0){
+		alert("you win");
+		games++;
+	}
+	else if (next==1){
+		alert("cpu wins");
+		games++;
+	}
+}
+
+
+
+function playAgain(again){
+	again=prompt("playing again? y for yes");
+	if(again=="y"){
+		return true;
+	}
+	else{
+		alert("goodbye");
+	}
+	
+}
